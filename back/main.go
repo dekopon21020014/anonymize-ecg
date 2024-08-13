477e4e8a3e0e89bb93d95b3c7c966bd972da9fb2
dekopon21020014 <renueta@icloud.com>
2024-08-14 01:43:08 +0900

    update: csvファイルを保存する関数をmain.goからcsv_controller.goに移行

--- back/main.go
+++ back/main.go
@@ -2,10 +2,8 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"log"
 	"os"
-	"path/filepath"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
@@ -38,7 +36,7 @@ func main() {
 
 	// `-export` が指定された場合はcsvに吐き出して終了
 	if *export {
-		err := saveCSVFile()
+		err := controller.SaveCSVFile()
 		if err != nil {
 			log.Fatalf("Error saving CSV file: %v", err)
 		}
@@ -61,49 +59,4 @@ func main() {
 
 	// サーバの起動
 	router.Run()
-}
-
-func saveCSVFile() error {
-	// データベースへの接続を取得
-	db, err := model.GetDB(os.Getenv("DSN"))
-	if err != nil {
-		return fmt.Errorf("failed to connect to database: %v", err)
-	}
-	defer db.Close() // データベース接続を確実に閉じる
-
-	// CSVデータをエクスポート
-	csv, err := model.ExportPatientsToCSV(db)
-	if err != nil {
-		return fmt.Errorf("failed to export patients to CSV: %v", err)
-	}
-
-	// 環境変数から保存先ディレクトリを取得
-	saveDir := os.Getenv("SAVE_DIR")
-	if saveDir == "" {
-		return fmt.Errorf("SAVE_DIR is not set")
-	}
-
-	// 保存先ディレクトリが存在しない場合は作成
-	if err := os.MkdirAll(saveDir, os.ModePerm); err != nil {
-		return fmt.Errorf("failed to create directory %s: %v", saveDir, err)
-	}
-
-	// 保存するファイルのフルパスを作成
-	filePath := filepath.Join(saveDir, csv.Name)
-
-	// ファイルを作成
-	file, err := os.Create(filePath)
-	if err != nil {
-		return fmt.Errorf("failed to create file %s: %v", filePath, err)
-	}
-	defer file.Close() // 閉じる処理を忘れずに追加
-
-	// ファイルにデータを書き込む
-	_, err = file.Write(csv.Content)
-	if err != nil {
-		return fmt.Errorf("failed to write to file %s: %v", filePath, err)
-	}
-
-	fmt.Printf("CSV file exported successfully to: %s\n", filePath)
-	return nil
 }
